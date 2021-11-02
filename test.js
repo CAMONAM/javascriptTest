c3ab72dc3206f67c1694a973b58ed302dd097697
Cam Sweetnam <77170374+CAMONAM@users.noreply.github.com>
2021-11-02 12:25:12 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 console.log('HELLO WORLD!!@! sumo cyco');
 var div = document.getElementById('scriptApp');
-div.innerHTML = "<div style='background-color:red; text-align:center; color:black'><p>HELLO SUMO CYCO WORLD!</p></div>";
+div.innerHTML = "<div style='background-color:red; text-align:center; color:black'><p>HELLO SUMO CYCO WORLD! ROCK ON!</p></div>";
